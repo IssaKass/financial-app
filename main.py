51491a13b09c0aec2a4f3bfbf01632af36324493
Abdallah-Issa Kass <issakass02@gmail.com>
2024-01-13 21:08:46 +0200

    Create db for testing

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 """Main script to run the Flask application."""
 
 from api import create_app
+from api.extensions import db
 
 if __name__ == "__main__":
-    APP = create_app(environment="development")
+    environment = "development"
+
+    APP = create_app(environment)
+
+    if environment == "testing":
+        with APP.app_context():
+            db.drop_all()
+            db.create_all()
+
     APP.run(debug=True, port=5000)
