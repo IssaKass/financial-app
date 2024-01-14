79bf29fbcb43b134f1ea5b60a1f85b653ddc1ced
Abdallah-Issa Kass <issakass02@gmail.com>
2024-01-14 16:14:06 +0200

    fix main.py

--- main.py
+++ main.py
@@ -4,37 +4,38 @@ import os
 from flask import Flask, jsonify
 from api import create_app
 
+app = Flask(__name__)
+
+
+@app.route("/")
+def test():
+    return jsonify(
+        {
+            "random": "56",
+            "random float": "87.247",
+            "bool": "false",
+            "date": "1989-07-08",
+            "regEx": "helloo to you",
+            "enum": "online",
+            "firstname": "Rani",
+            "lastname": "Kat",
+            "city": "Saint-Denis",
+            "country": "Panama",
+            "countryCode": "SY",
+            "email uses current data": "Rani.Kat@gmail.com",
+            "email from expression": "Rani.Kat@yopmail.com",
+            "array": ["Madelle", "Amara", "Mallory", "Quintina", "Oona"],
+            "array of objects": [
+                {"index": "0", "index start at 5": "5"},
+                {"index": "1", "index start at 5": "6"},
+                {"index": "2", "index start at 5": "7"},
+            ],
+            "Catrina": {"age": "49"},
+        }
+    )
+
+
 if __name__ == "__main__":
-    APP = create_app("development")
+    # APP = create_app("development")
 
-    APP = Flask(__name__)
-
-    APP.route("/")
-
-    def test():
-        return jsonify(
-            {
-                "random": "56",
-                "random float": "87.247",
-                "bool": "false",
-                "date": "1989-07-08",
-                "regEx": "helloo to you",
-                "enum": "online",
-                "firstname": "Rani",
-                "lastname": "Kat",
-                "city": "Saint-Denis",
-                "country": "Panama",
-                "countryCode": "SY",
-                "email uses current data": "Rani.Kat@gmail.com",
-                "email from expression": "Rani.Kat@yopmail.com",
-                "array": ["Madelle", "Amara", "Mallory", "Quintina", "Oona"],
-                "array of objects": [
-                    {"index": "0", "index start at 5": "5"},
-                    {"index": "1", "index start at 5": "6"},
-                    {"index": "2", "index start at 5": "7"},
-                ],
-                "Catrina": {"age": "49"},
-            }
-        )
-
-    APP.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(debug=True, port=os.getenv("PORT", default=5000))
