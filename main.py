50104279cb63389b129bfe2afb0f0d94ba1c6637
Abdallah-Issa Kass <issakass02@gmail.com>
2024-01-14 15:48:49 +0200

    Current changes

--- main.py
+++ main.py
@@ -2,16 +2,8 @@
 
 import os
 from api import create_app
-from api.extensions import db
 
 if __name__ == "__main__":
-    environment = os.getenv("FLASK_ENV", "development")
+    APP = create_app("development")
 
-    APP = create_app(environment)
-
-    if environment == "testing":
-        with APP.app_context():
-            db.drop_all()
-            db.create_all()
-
-    APP.run(debug=True, port=5000)
+    APP.run(debug=True, port=os.getenv("PORT", default=5000))
